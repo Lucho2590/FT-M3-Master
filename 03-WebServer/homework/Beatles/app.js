d469b59133b23a631537aadd8403307fd89c1834
Lucho2590 <lopezlucianomartin@gmail.com>
2022-11-03 19:20:31 -0300

    WebSerber Terminado

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -1,24 +1,61 @@
 var http = require('http');
-var fs   = require('fs');
+var fs = require('fs');
 
-var beatles=[{
-  name: "John Lennon",
-  birthdate: "09/10/1940",
-  profilePic:"https://blogs.correiobraziliense.com.br/trilhasonora/wp-content/uploads/sites/39/2020/10/CBNFOT081020100047-550x549.jpg"
-},
-{
-  name: "Paul McCartney",
-  birthdate: "18/06/1942",
-  profilePic:"http://gazettereview.com/wp-content/uploads/2016/06/paul-mccartney.jpg"
-},
-{
-  name: "George Harrison",
-  birthdate: "25/02/1946",
-  profilePic:"https://canaldosbeatles.files.wordpress.com/2012/02/george-george-harrison-8321345-438-600.jpg"
-},
-{
-  name: "Richard Starkey",
-  birthdate: "07/08/1940",
-  profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
-}
-]
+var beatles = [{
+        name: "John Lennon",
+        birthdate: "09/10/1940",
+        profilePic: "https://blogs.correiobraziliense.com.br/trilhasonora/wp-content/uploads/sites/39/2020/10/CBNFOT081020100047-550x549.jpg"
+    },
+    {
+        name: "Paul McCartney",
+        birthdate: "18/06/1942",
+        profilePic: "http://gazettereview.com/wp-content/uploads/2016/06/paul-mccartney.jpg"
+    },
+    {
+        name: "George Harrison",
+        birthdate: "25/02/1946",
+        profilePic: "https://canaldosbeatles.files.wordpress.com/2012/02/george-george-harrison-8321345-438-600.jpg"
+    },
+    {
+        name: "Richard Starkey",
+        birthdate: "07/08/1940",
+        profilePic: "http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
+    }
+];
+
+http.createServer((req, res) => {
+
+    // if (req.url.substr(0, 5) === '/api/') {
+    //     const beatle = req.url.split('/').pop()
+    //     const found = beatles.find(b => encodeURI(b.name).toLowerCase() === beatle.toLowerCase())
+    //     if (found) {
+    //         res.writeHead(200, { 'Content-Type': 'application/json' })
+    //         return res.end(JSON.stringify(found))
+    //     } else {
+    //         res.writeHead(404, { 'Content-Type': 'text/plain' })
+    //         return res.end(`${beatle} is not a beatle`)
+    //     }
+    // }
+
+    // if (req.url === '/api' || req.url === '/api/') {
+    //     res.writeHead(200, { 'Content-Type': 'application/json' });
+    //     res.end(JSON.stringify(beatles));
+    // }
+    if (req.url.substring(0, 5) === '/api/' && req.url.length > 5) {
+        let findBeatle = req.url.split('/').pop();
+        let foundBeatle = beatles.find((b) => findBeatle === encodeURI(b.name))
+        if (foundBeatle) {
+            res.writeHead(200, { 'Content-type': 'application/json' });
+            res.end(JSON.stringify(foundBeatle))
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/plain' })
+            res.end('No existe ese chango')
+        }
+    }
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-type': 'text/html' })
+        const index = fs.readFile(`./index.html`)
+        res.end( /*fs.readFile(__dirname + '/index.html')*/ index);
+    }
+
+}).listen(1337, '127.0.0.1');
--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,62 @@
-var fs  = require("fs")
-var http  = require("http")
+var fs = require("fs")
+var http = require("http")
 
 // Escribí acá tu servidor
+http.createServer((req, res) => {
+
+
+    fs.readFile(`./images${req.url}.jpg`, function(err, data) {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' })
+            res.end('Imagen not found')
+        } else {
+            res.writeHead(200, { 'Content-type': 'image' })
+            res.end(data)
+        }
+    })
+
+
+    fs.readFile(`${__dirname}/images/${req.url}.jpg`, ((err, data) => {
+        if (err) {
+            res.writeHead(404, { "Content-type": "text/plain" });
+            res.end('Exploto');
+        } else {
+            res.writeHead(200, { "Content-type": "image/jpg" })
+            res.end(data)
+        }
+    }))
+
+    // if (req.url === '/arcoiris_doge') {
+    //     res.writeHead(200, { 'Content-Type': 'image/jpg' })
+    //     var imagen = fs.readFileSync(`${__dirname}` + '/images/arcoiris_doge.jpg');
+    //     res.end(imagen);
+    // }
+    // if (req.url === '/badboy_doge') {
+    //     res.writeHead(200, { 'Content-Type': 'image/jpg' })
+    //     var imagen = fs.readFile(__dirname + '/images/badboy_doge.jpg');
+    //     res.end(imagen);
+    // }
+    // if (req.url === '/code_doge') {
+    //     res.writeHead(200, { 'Content-Type': 'image/jpg' })
+    //     var imagen = fs.readFile(__dirname + '/images/code_doge.jpg');
+    //     res.end(imagen);
+    // }
+    // if (req.url === '/resaca_doge') {
+    //     res.writeHead(200, { 'Content-Type': 'image/jpg' })
+    //     var imagen = fs.readFile(__dirname + '/images/resaca_doge.jpg');
+    //     res.end(imagen);
+    // }
+    // if (req.url === '/retrato_doge') {
+    //     res.writeHead(200, { 'Content-Type': 'image/jpg' })
+    //     var imagen = fs.readFile(__dirname + '/images/retrato_doge.jpg');
+    //     res.end(imagen);
+    // }
+    // if (req.url === '/sexy_doge') {
+    //     res.writeHead(200, { 'Content-Type': 'image/jpg' })
+    //     var imagen = fs.readFile(__dirname + '/images/sexy_doge.jpg');
+    //     res.end(imagen);
+    // } else {
+    //     res.writeHead(404, { 'Content-Type': 'text/plain' }); //Ponemos el status del response a 404: Not Found
+    //     res.end('exploto'); //No devolvemos nada más que el estado.
+    // }
+}).listen(1337, '127.0.0.1');
